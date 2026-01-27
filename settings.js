da38bb9598f2717aa2c1702eac6315455b388328
Adsani <s.suryarifandi@gmail.com>
2026-01-27 19:00:02 +0700

    Rebrand the project to be mine

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  author: 'mruniquehacker',
+  botName: "Adsani Bot",
+  botOwner: 'Adsani', // Your name
+  ownerNumber: '628157057607', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Test doang",
   version: "3.0.6",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
